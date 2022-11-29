22d90e28ce726e98507beb69826363b74084fb63
Tordar Tømmervik <tordar.tommervik@redcross.no>
2022-11-29 13:17:26 +0100

    adds num visits to menu

--- sats-visualise-map/src/Menu.js
+++ sats-visualise-map/src/Menu.js
@@ -1,13 +1,37 @@
 import React, {useState} from 'react'
 import json from './personalVisits.json'
 
+
+    
+
+
+
 const Menu = () => {
 
+    const ele = (element) => {
+        console.log(element)
+    }
+    
+
     return (
+        
         <div id="overlay">
+        <div className="address">
+        <div className="name">
+                Center
+                </div>
+                <div className="visits">
+                Number of visits
+                </div>
+            </div>
             {json.map(el=>
-            <div className="address">
-                {el.CENTER_ADDRESS}
+            <div className="address" onClick={() => ele(el.CENTER_ID)}>
+                <div className="name">
+                {el.CENTER_NAME}
+                </div>
+                <div className="visits">
+                {el.COUNT_VISITED}
+                </div>
             </div>
             )}
         </div>
