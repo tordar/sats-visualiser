071121b94670ce35b068af226566dc67b22c148d
Tordar Tømmervik <tordar.tommervik@gmail.com>
2024-11-07 14:00:16 +0100

    removed unused vars

--- sats-visualise-map/src/App.js
+++ sats-visualise-map/src/App.js
@@ -1,76 +1,54 @@
 import './App.css';
-import React, { useState } from 'react';
-import { MapContainer, TileLayer, Circle, Popup, useMap } from 'react-leaflet'
+import React from 'react';
+import { MapContainer, TileLayer, Circle, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
 import Menu from './Menu';
 import json from './personalVisits.json'
-import checkins from './CHECKINS.json'
 
 
 function App() {
-  const [geoLocation, setGeoLocations] = useState([])
-
-  const location=json
-  const checkinsData=checkins
-
-
-    {checkinsData.map(el =>{
-      if(el.CHECKIN_CENTER === json.CENTER_ID){
-        console.log(json.CENTER_NAME)
-      } else{
-        console.log('no')
-      }
-    })}
 
 
   return (
-    
-    
     <div id='map'>
-     <Menu>
-      
-     </Menu>
-  <MapContainer className='leaflet-container' center={[59.933, 10.750]} zoom={13} scrollWheelZoom={true}>
-  <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-
-
-
-  {location.map(element => {
-  if(element.COUNT_VISITED !== undefined){
-
-  return (
-    <Circle
-      center={element.GEOLOCATION}
-      key={element.CENTER_NAME}
-      radius={100}
-      color={'green'}
-      >
-      <Popup>
-        {`${element.CENTER_NAME}, has been visited ${element.COUNT_VISITED} times`}
-      </Popup>
-    </Circle> )
-    }
-  else {
-    return (
-      <Circle
-        center={element.GEOLOCATION}
-        key={element.CENTER_NAME}
-        radius={100}
-        color={'red'}
-        >
-        <Popup>
-          {`${element.CENTER_NAME}, has been visited 0 times`}
-        </Popup>
-      </Circle> )
-      }
-    }
-  )}
-    
-</MapContainer>
-</div>
+      <Menu>
+      </Menu>
+      <MapContainer className='leaflet-container' center={[59.933, 10.750]} zoom={13} scrollWheelZoom={true}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {json.map(element => {
+          if (element.COUNT_VISITED !== undefined) {
+            return (
+              <Circle
+                center={element.GEOLOCATION}
+                key={element.CENTER_NAME}
+                radius={100}
+                color={'green'}
+              >
+                <Popup>
+                  {`${element.CENTER_NAME}, has been visited ${element.COUNT_VISITED} times`}
+                </Popup>
+              </Circle>)
+          }
+          else {
+            return (
+              <Circle
+                center={element.GEOLOCATION}
+                key={element.CENTER_NAME}
+                radius={100}
+                color={'red'}
+              >
+                <Popup>
+                  {`${element.CENTER_NAME}, has been visited 0 times`}
+                </Popup>
+              </Circle>)
+          }
+        }
+      )}
+      </MapContainer>
+    </div>
   );
 }
 
--- sats-visualise-map/src/Menu.js
+++ sats-visualise-map/src/Menu.js
@@ -1,26 +1,27 @@
 import React, {useState} from 'react'
 import json from './personalVisits.json'
 
-
-    
-
-
-
 const Menu = () => {
+  const [isActive, setActive] = useState("none");
 
-    const addCountVisited = (element) => {
+    const addCountVisited = () => {
         json.map(el => {
             if(!el.COUNT_VISITED){
                 el.COUNT_VISITED = 0
             }
+            return el
         })
     }  
     
     addCountVisited()
 
+    const showMenu = () => {
+        setActive("block");
+    }
+
     return (
         
-        <div id="overlay">
+        <div id="overlay" className={isActive ? "display" : null} onClick={showMenu}>
         <div className="address">
         <div className="name">
                 Center
